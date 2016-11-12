362504ac1e5c522f4cc70b6f92084cc125d869b5
metalerk <rodriguezjluis0@gmail.com>
2016-11-11 18:06:21 -0600

    First commit

--- service.py
+++ service.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
 
 def handler(event, context):
 
     nombre = event.get('nombre')
     edad = event.get('edad')
 
-    return "Tu nombre es {} y tienes {}".format(nombre, str(edad))
+    return json.dumps({"received":True})
